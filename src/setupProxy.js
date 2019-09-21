7be9cdd0fec79a1c90d1067c2b78bd4de5f168dd
WilliamCui31 <444738577@qq.com>
2019-09-21 16:00:28 +0800

    添加主要国家的api代理

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,12 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(proxy('/hk', { target: 'https://itunes.apple.com', changeOrigin: true }));
   app.use(proxy('/cn', { target: 'https://itunes.apple.com', changeOrigin: true }));
+  app.use(proxy('/hk', { target: 'https://itunes.apple.com', changeOrigin: true }));
+  app.use(proxy('/tw', { target: 'https://itunes.apple.com', changeOrigin: true }));
+  app.use(proxy('/ma', { target: 'https://itunes.apple.com', changeOrigin: true }));
+  app.use(proxy('/jp', { target: 'https://itunes.apple.com', changeOrigin: true }));
+  app.use(proxy('/us', { target: 'https://itunes.apple.com', changeOrigin: true }));
+  app.use(proxy('/kr', { target: 'https://itunes.apple.com', changeOrigin: true }));
+  app.use(proxy('/sg', { target: 'https://itunes.apple.com', changeOrigin: true }));
 };
